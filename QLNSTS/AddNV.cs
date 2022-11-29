0ace13567045d3b1e2a2cd2a8c56631ee62eacbe
KT190823 <101693059+KT190823@users.noreply.github.com>
2022-11-29 21:54:30 +0700

    Add files via upload

---
+++ QLNSTS/AddNV.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using QLNSTS.Models;
+
+namespace QLNSTS
+{
+    public partial class FormAddNV : Form
+    {
+        QLNSEntities database = new QLNSEntities();
+        public FormAddNV()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int id = Convert.ToInt32(txtID.Value);
+            int pos = (comboBox1.Text == "Nhân viên") ? 0 : 1;
+            bool sex = (radioButton1.Checked) ? true : false;
+            var user = new ThongTinNhanVien()
+            {
+                Id = id,
+                TenNV = txtName.Text,
+                SDT = txtSDT.Text,
+                NgaySinh = NgaySinh.Value,
+                Diachi = txtAddress.Text,
+                GioiTinh = sex,
+                ChucVu = pos,
+                MaNV = id.ToString() + pos.ToString(),
+            };
+            database.ThongTinNhanViens.Add(user);
+            database.SaveChanges();
+        }
+    }
+}
---
+++ QLNSTS/Home.Designer.cs
@@ -0,0 +1,143 @@
+﻿
+namespace QLNSTS
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox = new System.Windows.Forms.GroupBox();
+            this.btnDS = new System.Windows.Forms.Button();
+            this.btnEdit = new System.Windows.Forms.Button();
+            this.btnView = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.groupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox
+            // 
+            this.groupBox.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox.Controls.Add(this.btnDS);
+            this.groupBox.Controls.Add(this.btnEdit);
+            this.groupBox.Controls.Add(this.btnView);
+            this.groupBox.Controls.Add(this.btnDelete);
+            this.groupBox.Controls.Add(this.btnAdd);
+            this.groupBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox.Location = new System.Drawing.Point(23, 22);
+            this.groupBox.Name = "groupBox";
+            this.groupBox.Size = new System.Drawing.Size(213, 482);
+            this.groupBox.TabIndex = 0;
+            this.groupBox.TabStop = false;
+            this.groupBox.Text = "Các chức năng";
+            // 
+            // btnDS
+            // 
+            this.btnDS.Location = new System.Drawing.Point(35, 385);
+            this.btnDS.Name = "btnDS";
+            this.btnDS.Size = new System.Drawing.Size(143, 54);
+            this.btnDS.TabIndex = 4;
+            this.btnDS.Text = "Xuất danh sách";
+            this.btnDS.UseVisualStyleBackColor = true;
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.Location = new System.Drawing.Point(35, 212);
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(143, 51);
+            this.btnEdit.TabIndex = 3;
+            this.btnEdit.Text = "Chỉnh sửa nhân viên";
+            this.btnEdit.UseVisualStyleBackColor = true;
+            this.btnEdit.Click += new System.EventHandler(this.btnEdit_Click);
+            // 
+            // btnView
+            // 
+            this.btnView.Location = new System.Drawing.Point(35, 289);
+            this.btnView.Name = "btnView";
+            this.btnView.Size = new System.Drawing.Size(143, 64);
+            this.btnView.TabIndex = 2;
+            this.btnView.Text = "Thông tin chi tiết nhân viên";
+            this.btnView.UseVisualStyleBackColor = true;
+            this.btnView.Click += new System.EventHandler(this.btnView_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(35, 134);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(143, 53);
+            this.btnDelete.TabIndex = 1;
+            this.btnDelete.Text = "Xóa nhân viên";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(35, 53);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(143, 55);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Thêm nhân viên";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.BackColor = System.Drawing.Color.LightCoral;
+            this.btnLogout.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogout.Location = new System.Drawing.Point(64, 574);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(137, 34);
+            this.btnLogout.TabIndex = 1;
+            this.btnLogout.Text = "Đăng xuất";
+            this.btnLogout.UseVisualStyleBackColor = false;
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 639);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.groupBox);
+            this.Name = "Home";
+            this.Text = "Form1";
+            this.groupBox.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox;
+        private System.Windows.Forms.Button btnDS;
+        private System.Windows.Forms.Button btnEdit;
+        private System.Windows.Forms.Button btnView;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
+
